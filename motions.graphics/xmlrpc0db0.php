757ed88a32e7d9c43127217724563136d2ca2b8b
Nicolas Gonzalez <nicolas.gonzalez93@outlook.com>
2019-02-19 17:15:56 -0300

    website

---
+++ motions.graphics/xmlrpc0db0.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>https://wordpress.org/</engineLink>
+    <homePageLink>http://motions.graphics</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://motions.graphics/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://motions.graphics/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://motions.graphics/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://motions.graphics/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://motions.graphics/wp-json/" />
+	    </apis>
+  </service>
+</rsd>
